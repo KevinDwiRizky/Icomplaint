925eea2899d30b9cbc0fa4f59f9d86f48a547eb8
Kevin Dwi Rizky <kevindwirizkyy@gmail.com>
2024-06-06 12:50:17 +0700

    add for auth header cors

--- src/main/java/org/kevin/resource/CategoryResource.java
+++ src/main/java/org/kevin/resource/CategoryResource.java
@@ -19,6 +19,7 @@ public class CategoryResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("ADMIN")
     public WebResponse<List<CategoryResponse>> getCategory() {
         List<CategoryResponse> categories = categoryService.getAllcategory();
         return WebResponse.<List<CategoryResponse>>builder()
